8993816c11d568532a35134216a894522df4c954
Ayyasamy Murugesan <CIPL0606@colanonline.local>
2018-07-24 21:02:29 +0530

    init commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './font.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ fontFamily: 'Titillium Web' }}>
+      <img src='img/poster1.jpg'/>
+      helloworld!!!</div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var config = {
+  entry: './main.js',
+
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+
+  devServer: {
+    inline: true,
+    ip: '0.0.0.0',
+    port: 7000
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.(scss|css)$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config;
